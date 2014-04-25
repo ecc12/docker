9a1e3d265f2cc539756384f950d9d31574de8b03
Cameron King <cking@ecc12.com>
2014-04-25 15:01:28 +0000

    preinstall packages for zimbra

--- zimbra/start.sh
+++ zimbra/start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-ls /opt/zimbra/config.install || (
+ls /opt/zimbra/installed-by-docker || (
+mkdir -p /opt/zimbra
+chown zimbra. /opt/zimbra
+mv /opt/.zimbra/{*,.*} /opt/zimbra/
 sed -i "s/XHOSTNAMEX/`hostname -f`/" /tmp/zcs/config.defaults
 sed -i "s/XPASSWORDX/`date | sha1sum | cut -c-8`/" /tmp/zcs/config.defaults
 sed -i "s/XPASSWORD2X/`date | sha1sum | cut -c-8`/" /tmp/zcs/config.defaults
@@ -8,13 +11,9 @@ sed -i "s/XRANDOMX/`date | sha1sum | cut -c-8`/" /tmp/zcs/config.defaults
 sed -i "s/XRANDOM2X/`date | sha1sum | cut -c-8`/" /tmp/zcs/config.defaults
 sed -i "s/XRANDOM3X/`date | sha1sum | cut -c-8`/" /tmp/zcs/config.defaults
 cp /tmp/zcs/config.defaults /opt/zimbra/config.install
-)
-
-ls /opt/zimbra/zmstat || (
-cd /tmp/zcs/zcs-*
-./install.sh -s --platform-override /opt/zimbra/config.install
 mv /opt/zimbra/.install_history{,.orig}
 /opt/zimbra/libexec/zmsetup.pl -d -c /opt/zimbra/config.install
+touch /opt/zimbra/installed-by-docker
 )
 
 /etc/init.d/sshd start
