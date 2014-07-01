c23f1d1a307bc4850ee31090b345eba837efe6c6
Cameron King <cking@ecc12.com>
2014-07-01 11:42:32 -0500

    move minecraft to branch

--- minecraft-1.7/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /minecraft
-[ -x bootstrap.sh ] && ./bootstrap.sh
-[ -x start.sh ] && exec runuser -s /bin/sh -c ./start.sh minecraft
-exec runuser -s /bin/sh -c '/usr/bin/java minecraft.jar' minecraft
-
